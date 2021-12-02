27886eba996b6f2db68f358dad2c763ab001938e
David Bründel <davidbrundel@MacBook-Pro-von-David.local>
2021-12-02 22:02:20 +0100

    Created prooject with create react app and added files homepgae

---
+++ src/homepage.component.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+
+const Homepage = () => (
+<div className='homepage'>
+    <h1>Welcome to my Homepage</h1>
+    <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>HATS</h1>
+                <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        
+
+        
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>Jackets</h1>
+                <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        
+
+        
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>SHOES</h1>
+                <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        
+
+        
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>WOMENS</h1>
+                <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        
+
+        
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>MENS</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
+        </div>
+    </div>
+    
+    </div>
+);
+
+export default Homepage;
+
+
