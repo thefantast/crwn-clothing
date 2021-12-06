e9742fd536e838b9f2c8a95cb57fa417413f2ad0
David Bründel <davidbrundel@MacBook-Pro-von-David.local>
2021-12-06 15:56:07 +0100

    adding header component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
+
 
 
   
@@ -14,10 +16,11 @@ import ShopPage from './pages/shop/shop.component.jsx';
 function App() {
   return (
     <div>
+    <Header />
+
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
-
 
       </Routes>
     </div>
