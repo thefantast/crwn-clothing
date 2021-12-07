04db99ba67000abc85b1bd8b0cb84fb84f5f0411
David Bründel <davidbrundel@MacBook-Pro-von-David.local>
2021-12-07 21:10:31 +0100

    implemnted a lot video 100 now you can use the firestore

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.componnt';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
   
@@ -26,10 +26,26 @@ class App extends React.Component {
 
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      
+      if(userAuth) {
+          const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+          userRef.onSnapshot(snapShot => {
+            
+            this.setState({ 
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            });
+            
+          });
+          
+
+      } 
+      this.setState({currentUser: userAuth});
+      
     });
   }
 
