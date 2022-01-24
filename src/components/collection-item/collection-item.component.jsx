f63795a4df5fea06c19eec38743146d82ea38faa
David Bründel <davidbrundel@MacBook-Pro-von-David.local>
2022-01-24 22:40:53 +0100

    created New cartitems for the marketplace

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import BlurOnIcon from '@mui/icons-material/BlurOn';
 
-
+import { Link } from 'react-router-dom';
 
 import './collection-item.styles.scss';
 
@@ -12,30 +13,40 @@ import { addItem } from '../../redux/cart/cart.actions';
 //functional Component
 
 const CollectionItem = ({ item, addItem}) => {
-    const {  name, price, imageUrl } = item;
+    const {  name, price, imageUrl, artistName } = item;
     return(
-    <div className='collection-item'>
         
+    <div className='collection-item'>
+    
         <div className='image'
             style={{ 
                 backgroundImage: `url(${imageUrl})`
             }}
-            
         >
+        
         </div>
+        
+        
 
         <div className='collection-footer'>
-                <span className='name'>{ name }</span>
-                <span className='price'>{ price }</span>
+                <h3 className='name'>{ name }</h3>
+                <span className='artistName'>{ artistName }</span>
+                <h4 className='price'> <BlurOnIcon />{ price }</h4>
+                  
                 
 
         </div>
+        
+        <div className="ButtonSection">
+          <Link to="/productPage">  <div className="ProductButton">Product Details</div> </Link>
 
         <CustomButton onClick={() => addItem(item)} inverted>
             Add to cart 
         </CustomButton>
+        </div>
 
     </div>
+    
     )};
 
     const mapDispatchToProps = dispatch => ({
--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -1,8 +1,8 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components'
 
-const buttonStyles = css`
-  background-color: black;
-  color: white;
+const buttonStyles = styled.div`
+  background-color: #A65EC6;
+  color: #000;
   border: none;
   &:hover {
     background-color: white;
@@ -11,8 +11,8 @@ const buttonStyles = css`
   }
 `;
 
-const invertedButtonStyles = css`
-  background-color: white;
+const invertedButtonStyles = styled.div`
+  background-color: #A65EC6;
   color: black;
   border: 1px solid black;
   &:hover {
@@ -22,7 +22,7 @@ const invertedButtonStyles = css`
   }
 `;
 
-const googleSignInStyles = css`
+const googleSignInStyles = styled.div`
   background-color: #4285f4;
   color: white;
   &:hover {
@@ -43,15 +43,79 @@ export const CustomButtonContainer = styled.button`
   min-width: 165px;
   width: auto;
   height: 50px;
-  letter-spacing: 0.5px;
-  line-height: 50px;
+  font-family: MigraExtraBold;
+  letter-spacing: 0.19em;
   padding: 0 35px 0 35px;
-  font-size: 15px;
   text-transform: uppercase;
-  font-family: 'Open Sans Condensed';
-  font-weight: bolder;
   cursor: pointer;
   display: flex;
   justify-content: center;
+  align-items: center;
+  justify-content: center;
   ${getButtonStyles}
+    
+    font-size: 15px;
+    width: 140px;
+    height: 40px;
+    border: none;
+    outline: none;
+    color: #fff;
+    background: #111;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 10px;
+    padding: 10px;
+
+
+        :before {
+
+            content: '';
+            background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+            position: absolute;
+            top: -2px;
+            left:-2px;
+            background-size: 400%;
+            z-index: -1;
+            filter: blur(5px);
+            width: calc(100% + 4px);
+            height: calc(100% + 4px);
+            animation: glowing 20s linear infinite;
+            opacity: 0;
+            transition: opacity .3s ease-in-out;
+            border-radius: 10px;
+        }
+
+        :active {
+            color: #000
+        }
+
+        :active:after {
+            background: transparent;
+        }
+
+        :hover:before {
+            opacity: 1;
+        }
+
+            :after {
+                z-index: -1;
+                content: '';
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                background: #111;
+                left: 0;
+                top: 0;
+                border-radius: 10px;
+            }
+
+            @keyframes glowing {
+                0% { background-position: 0 0; }
+                50% { background-position: 400% 0; }
+                100% { background-position: 0 0; }
+            }
+
+
+            
 `;
